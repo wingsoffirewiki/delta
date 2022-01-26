356a834bc100e1fb7518fc18b36b241ab7449921
Ferotiq <evanrayweigel@outlook.com>
2022-01-26 16:11:36 -0600

    Reformat

--- src/commands/Database Commands/get.ts
+++ src/commands/Database Commands/get.ts
@@ -1,3 +1,5 @@
+/** @format */
+
 import { Command } from "fero-dc";
 import { Guild, IGuild } from "../../models/Guild";
 import { MessageEmbed } from "discord.js";
@@ -19,44 +21,55 @@ export default new Command({
 
     const guild = context.guild;
 
-    const guildModel: IGuild = await Guild.findOne({
-      _id: guild.id
-    }, null, { upsert: true });
+    const guildModel: IGuild = await Guild.findOne(
+      {
+        _id: guild.id
+      },
+      null,
+      { upsert: true }
+    );
 
     const embed = new MessageEmbed();
 
-    embed.setTitle("Delta: Server Settings")
-      .setDescription(`The following are the settings for the server \`${guild.name}\``)
+    embed
+      .setTitle("Delta: Server Settings")
+      .setDescription(
+        `The following are the settings for the server \`${guild.name}\``
+      )
       .setAuthor({
         name: context.author.username,
         iconURL: context.author.avatarURL({ dynamic: true }) || ""
       })
       .setColor("RANDOM")
       .setThumbnail(guild.iconURL({ dynamic: true }) || "")
-      .addFields(
-        [
-          {
-            name: "Channels",
-            value: `Logs: \`${guildModel.channelIDs.logs}\`\nMod Logs: \`${guildModel.channelIDs.modLogs}\`\nAdmin Logs: \`${guildModel.channelIDs.adminLogs}\`\nFunnies: \`${guildModel.channelIDs.funnies}\``,
-            inline: true
-          },
-          {
-            name: "Roles",
-            value: `Mods: ${guildModel.roleIDs.mods.map(v => guild.roles.cache.get(v)?.name || "`No name or role doesn't exist`").join("\n")}`,
-            inline: true
-          },
-          {
-            name: "Messages",
-            value: `Verification ID: \`${guildModel.messages.verification.id}\`\nVerification Channel ID: \`${guildModel.messages.verification.id}\``,
-            inline: true
-          },
-          {
-            name: "Enabled Features",
-            value: `Economy: \`${guildModel.features.scales}\`\nLogging: \`${guildModel.features.logging}\`\nMod Logging: \`${guildModel.features.modLogging}\`\nAdmin Logging: \`${guildModel.features.adminLogging}\`\nModeration: \`${guildModel.features.moderation}\`\nFunnies: \`${guildModel.features.funnies}\``,
-            inline: true
-          }
-        ]
-      )
+      .addFields([
+        {
+          name: "Channels",
+          value: `Logs: \`${guildModel.channelIDs.logs}\`\nMod Logs: \`${guildModel.channelIDs.modLogs}\`\nAdmin Logs: \`${guildModel.channelIDs.adminLogs}\`\nFunnies: \`${guildModel.channelIDs.funnies}\``,
+          inline: true
+        },
+        {
+          name: "Roles",
+          value: `Mods: ${guildModel.roleIDs.mods
+            .map(
+              v =>
+                guild.roles.cache.get(v)?.name ||
+                "`No name or role doesn't exist`"
+            )
+            .join("\n")}`,
+          inline: true
+        },
+        {
+          name: "Messages",
+          value: `Verification ID: \`${guildModel.messages.verification.id}\`\nVerification Channel ID: \`${guildModel.messages.verification.id}\``,
+          inline: true
+        },
+        {
+          name: "Enabled Features",
+          value: `Economy: \`${guildModel.features.scales}\`\nLogging: \`${guildModel.features.logging}\`\nMod Logging: \`${guildModel.features.modLogging}\`\nAdmin Logging: \`${guildModel.features.adminLogging}\`\nModeration: \`${guildModel.features.moderation}\`\nFunnies: \`${guildModel.features.funnies}\``,
+          inline: true
+        }
+      ])
       .setTimestamp()
       .setFooter({
         text: "Delta, The Wings of Fire Moderation Bot",
@@ -67,6 +80,5 @@ export default new Command({
       ephemeral: true,
       embeds: [embed]
     });
-
   }
 });
