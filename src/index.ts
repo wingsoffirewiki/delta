867f6ed8c9690eced2383bfba4aeb32d73bb508e
Ferotiq <evanrayweigel@outlook.com>
2023-02-04 12:29:42 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-console.clear();
-
 import { Client, ClientOptions } from "fero-dc";
 
 import options from "./config/config.json";
 
 import { config } from "dotenv";
-
 config();
 
 const client = new Client(options as ClientOptions, __dirname);
 
-client.start(process.env.TOKEN as string);
+const token = process.env.TOKEN;
+if (token === undefined) {
+  throw new Error("Missing Discord token.");
+}
+
+client.start(token);
