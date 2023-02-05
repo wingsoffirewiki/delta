43313ed3228e6ef09a2e78ec36266a56105f04e2
Ferotiq <evanrayweigel@outlook.com>
2023-02-04 18:22:49 -0600

    update fero-dc

---
+++ src/listeners/on-chat-command.ts
@@ -0,0 +1,29 @@
+import { EventListener } from "fero-dc";
+
+export default new EventListener<"interactionCreate">()
+  .setEvent("interactionCreate")
+  .setListener(async (client, interaction) => {
+    if (!interaction.isChatInputCommand()) {
+      return;
+    }
+
+    const guild = interaction.guild;
+    const member = interaction.member;
+
+    if (guild === null || member === null) {
+      return;
+    }
+
+    const command = client.commands.get(interaction.commandName);
+    if (command === undefined) {
+      interaction.reply({
+        ephemeral: true,
+        fetchReply: false,
+        content: `Command ${interaction.commandName} does not exist on this bot!`
+      });
+
+      return;
+    }
+
+    command.data.run(client, interaction);
+  });
---
+++ src/listeners/on-verify-button-click.ts
@@ -0,0 +1,84 @@
+import { EventListener } from "fero-dc";
+import { prisma } from "../db";
+
+export default new EventListener<"interactionCreate">()
+  .setEvent("interactionCreate")
+  .setListener(async (client, interaction) => {
+    if (!interaction.isButton()) {
+      return;
+    }
+
+    const guild = interaction.guild;
+
+    if (guild === null) {
+      return;
+    }
+
+    if (interaction.customId !== "verify") {
+      return;
+    }
+
+    const guildModel = await prisma.guild.findUnique({
+      where: {
+        id: guild.id
+      }
+    });
+
+    if (guildModel === null) {
+      return;
+    }
+
+    if (guildModel.messages.verification.id === "000000000000000000") {
+      return;
+    }
+
+    const verificationMessage = guildModel.messages.verification;
+
+    if (
+      interaction.channelId !== verificationMessage.channelID ||
+      interaction.message.id !== verificationMessage.id
+    ) {
+      return;
+    }
+
+    const roleID = guildModel.roleIDs.verified;
+
+    if (roleID === "000000000000000000") {
+      return;
+    }
+
+    const role = await guild.roles.fetch(roleID, {
+      cache: true
+    });
+
+    const member = await guild.members.fetch(interaction.user.id);
+
+    if (role === null || member === null) {
+      return;
+    }
+
+    if (member.roles.cache.has(role.id)) {
+      interaction.reply({
+        ephemeral: true,
+        content: "You are already verified!"
+      });
+
+      return;
+    }
+
+    try {
+      await member.roles.add(role);
+
+      interaction.reply({
+        ephemeral: true,
+        content: "You have been verified!"
+      });
+    } catch (error) {
+      interaction.reply({
+        ephemeral: true,
+        content: "An error occurred while trying to verify you."
+      });
+
+      console.error(error);
+    }
+  });
