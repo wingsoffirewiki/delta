b619c98868b3f9e3d1b44d664be43e0de2881894
Ferotiq <evanrayweigel@outlook.com>
2023-02-08 17:03:07 -0600

    use PascalCase for LogType

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,14 @@
-import type { User } from "discord.js";
 
 export enum LogType {
-  BAN,
-  TEMPORARY_BAN,
-  TIMEOUT,
-  UNBAN,
-  WARN,
-  KICK,
-  MESSAGE_EDIT,
-  MESSAGE_DELETE,
-  BULK_MESSAGE_DELETE,
-  BANNED_WORD_DETECTED
+  Ban,
+  TemporaryBan,
+  Timeout,
+  Unban,
+  Warn,
+  Kick,
+  MessageEdit,
+  MessageDelete,
+  BulkMessageDelete,
+  BannedWordDetected
+}
 }
