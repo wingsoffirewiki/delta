1e57f0b394eb804c5365f9b57d5d9bdc38b1a5e7
Evan <evanrayweigel@outlook.com>
2023-02-08 17:53:12 -0600

    Create message-create.ts

---
+++ src/listeners/message-create.ts
@@ -0,0 +1,58 @@
+import { EventListener } from "fero-dc";
+import { prisma } from "../db";
+
+const HONK_EMOJI_ID = "639271354734215178";
+
+export default new EventListener<"messageCreate">()
+  .setEvent("messageCreate")
+  .setListener(async (client, message) => {
+    if (message.author.bot) {
+      return;
+    }
+
+    if (message.guild === null) {
+      return;
+    }
+
+    const lowerCaseMessageContent = message.content.toLowerCase();
+
+    if (lowerCaseMessageContent.includes("goose")) {
+      message.react("🦢").catch(console.log);
+    }
+
+    if (lowerCaseMessageContent.includes("honk")) {
+      message.react(HONK_EMOJI_ID).catch(console.log);
+    }
+
+    const guildModel = await prisma.guild.findUnique({
+      where: {
+        id: message.guild.id
+      }
+    });
+
+    if (guildModel === null) {
+      console.log("Guild not found in the database");
+
+      return;
+    }
+
+    if (!guildModel.features.scales) {
+      return;
+    }
+
+    const randomAmount = Math.floor(Math.random() * 50) + 1;
+
+    await prisma.user
+      .updateMany({
+        where: {
+          id: message.author.id,
+          banned: false
+        },
+        data: {
+          scales: {
+            increment: randomAmount
+          }
+        }
+      })
+      .catch(console.log);
+  });
