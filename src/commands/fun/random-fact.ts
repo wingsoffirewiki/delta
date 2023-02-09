6b893e6c23eaffef19acab03581f63fd38a21a29
Evan <evanrayweigel@outlook.com>
2023-02-09 17:54:40 -0600

    Create random-fact.ts

---
+++ src/commands/fun/random-fact.ts
@@ -0,0 +1,46 @@
+import { Command } from "fero-dc";
+import { getRandomFact } from "../../util/fact-api";
+import { Colors, EmbedBuilder } from "discord.js";
+
+export default new Command()
+  .setName("random-fact")
+  .setDescription("Gets a random fact")
+  .setCategory("Fun")
+  .setRun(async (client, interaction) => {
+    await interaction.deferReply({
+      ephemeral: false,
+      fetchReply: false
+    });
+
+    const randomFact = await getRandomFact();
+    if (randomFact === undefined) {
+      interaction.followUp({
+        content: "Failed to get random fact"
+      });
+
+      return;
+    }
+
+    const factText = randomFact.text.replace(/`/g, "'");
+    const permalink = randomFact.permalink;
+
+    const embed = new EmbedBuilder()
+      .setTitle("Delta: Random Fact")
+      .setURL(permalink)
+      .setColor(Colors.Blurple)
+      .setDescription(
+        `If this fact seems false, please refer to the [URL](${permalink}) to verify its sources.`
+      )
+      .addFields({
+        name: "Random Fact",
+        value: `\`${factText}\``
+      })
+      .setTimestamp()
+      .setFooter({
+        text: "Delta, The Wings of Fire Moderation Bot",
+        iconURL: client.user.avatarURL() ?? ""
+      });
+    interaction.followUp({
+      embeds: [embed]
+    });
+  });
