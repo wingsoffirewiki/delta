d0c29360a98dd5cc416cbf7375197d439c1779cd
Evan <evanrayweigel@outlook.com>
2023-02-08 20:20:36 -0600

    Create on-member-leave.ts

---
+++ src/listeners/on-member-leave.ts
@@ -0,0 +1,45 @@
+import { EmbedBuilder } from "@discordjs/builders";
+import { Colors } from "discord.js";
+import { EventListener } from "fero-dc";
+import { prisma } from "../util/db";
+
+export default new EventListener<"guildMemberRemove">()
+  .setEvent("guildMemberRemove")
+  .setListener(async (client, member) => {
+    const guild = member.guild;
+
+    const guildModel = await prisma.guild.findUnique({
+      where: {
+        id: guild.id
+      }
+    });
+    if (guildModel === null) {
+      console.log("Guild not found in the database");
+
+      return;
+    }
+
+    const logsChannel = guild.channels.cache.get(guildModel.channelIds.logs);
+    if (
+      logsChannel === undefined ||
+      !logsChannel.isTextBased() ||
+      !guildModel.features.logging
+    ) {
+      return;
+    }
+
+    const embed = new EmbedBuilder()
+      .setTitle("Delta: Member Left")
+      .setColor(Colors.Red)
+      .setDescription(`\`${member.user.username}\` has left \`${guild.name}\``)
+      .setAuthor({
+        name: `${member.user.tag} (${member.id})`,
+        iconURL: member.user.avatarURL() ?? ""
+      })
+      .setTimestamp()
+      .setFooter({
+        text: "Delta, The Wings of Fire Moderation Bot",
+        iconURL: client.user.avatarURL() ?? ""
+      });
+    logsChannel.send({ embeds: [embed] });
+  });
