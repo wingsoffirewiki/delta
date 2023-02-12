498cef9812328c997ce884e8072d55b4c365bdfa
Evan <evanrayweigel@outlook.com>
2023-02-12 14:31:50 -0600

    add info command

---
+++ src/util/strings.ts
@@ -0,0 +1,6 @@
+export function toPascalCase(string: string, separator?: string): string {
+  return string
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.substring(1))
+    .join(separator);
+}
