6d3c41f258afad4c982e8a2ba0d5959ea6411542
Evan <evanrayweigel@outlook.com>
2023-02-12 13:41:07 -0600

    add random-funnie

---
+++ src/commands/funnies/random-funnie.ts
@@ -0,0 +1,130 @@
+import { Command } from "fero-dc";
+import { prisma } from "../../util/prisma-client";
+import { randomElement } from "../../util/random";
+import { Colors, EmbedBuilder } from "discord.js";
+import { getFunnieReactionCounts } from "../../util/funnies";
+
+export default new Command()
+  .setName("random-funnie")
+  .setDescription("Get a random funnie (starboard) from the database")
+  .setCategory("Funnies")
+  .setRun(async (client, interaction) => {
+    await interaction.deferReply({
+      fetchReply: false
+    });
+
+    const guild = interaction.guild;
+    if (guild === null) {
+      await interaction.followUp({
+        content: "This command can only be used in a server."
+      });
+
+      return;
+    }
+    const guildModel = await prisma.guild.findUnique({
+      where: {
+        id: guild.id
+      }
+    });
+    if (guildModel === null) {
+      await interaction.followUp({
+        content: "This server has not been initialized."
+      });
+
+      return;
+    }
+    if (!guildModel.features.funnies) {
+      await interaction.followUp({
+        content: "Funnies are not enabled in this server."
+      });
+
+      return;
+    }
+
+    const funnieUpvoteEmoji = guildModel.emojis.funnieUpvote;
+    const funnieModUpvoteEmoji = guildModel.emojis.funnieModUpvote;
+
+    const funnies = await prisma.funnie.findMany({
+      where: {
+        guildId: guild.id
+      }
+    });
+    if (funnies.length === 0) {
+      await interaction.followUp({
+        content: "There are no funnies in this server."
+      });
+
+      return;
+    }
+
+    const funnie = randomElement(funnies);
+    const funnieChannel = await guild.channels
+      .fetch(funnie.channelId)
+      .catch(() => null);
+    if (funnieChannel === null || !funnieChannel.isTextBased()) {
+      await interaction.followUp({
+        content: "Failed to get funnie channel."
+      });
+
+      return;
+    }
+    const funnieMessage = await funnieChannel.messages
+      .fetch(funnie.messageId)
+      .catch(() => null);
+    if (funnieMessage === null) {
+      await interaction.followUp({
+        content: "Failed to get funnie message."
+      });
+
+      return;
+    }
+    const funnieMember = funnieMessage.member;
+
+    const [funnieUpvoteCount, funnieModUpvoteCount] =
+      await getFunnieReactionCounts(guildModel, funnieMessage);
+
+    const author = interaction.user;
+
+    const embed = new EmbedBuilder()
+      .setTitle(`${guild.name}: #${funnieChannel.name}`)
+      .setDescription(
+        `${funnieMessage.content ?? "No Content."}
+        
+        [Jump to Message](${funnieMessage.url})`
+      )
+      .setURL(funnieMessage.url)
+      .setColor(funnieMember?.displayColor || Colors.Blurple)
+      .setAuthor({
+        name: funnieMember?.displayName ?? author.username,
+        iconURL:
+          funnieMember?.user.displayAvatarURL() ?? author.avatarURL() ?? ""
+      })
+      .addFields(
+        {
+          name: funnieUpvoteEmoji,
+          value: funnieUpvoteCount.toString(),
+          inline: true
+        },
+        {
+          name: funnieModUpvoteEmoji,
+          value: funnieModUpvoteCount.toString(),
+          inline: true
+        }
+      )
+      .setTimestamp(funnieMessage.createdAt)
+      .setFooter({
+        text: "Delta, The Wings of Fire Moderation Bot",
+        iconURL: client.user.avatarURL() ?? ""
+      });
+
+    if (funnieMessage.attachments.size > 0) {
+      const firstAttachment = funnieMessage.attachments.first();
+      if (firstAttachment !== undefined) {
+        embed.setImage(firstAttachment.url);
+      }
+    }
+
+    await interaction.followUp({
+      embeds: [embed]
+    });
+  });
