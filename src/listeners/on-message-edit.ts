c4f9dc209c10226c20f231565a29d9e392ae5333
Evan <evanrayweigel@outlook.com>
2023-02-12 18:15:00 -0600

    Create on-message-edit.ts

---
+++ src/listeners/on-message-edit.ts
@@ -0,0 +1,27 @@
+import { EventListener } from "fero-dc";
+import { log } from "../util/logging";
+import { LogType } from "../util/types";
+
+export default new EventListener<"messageUpdate">()
+  .setEvent("messageUpdate")
+  .setListener(async (client, oldMessage, newMessage) => {
+    const fetchedNewMessage = await newMessage.fetch(true);
+
+    const guild = fetchedNewMessage.guild;
+    if (
+      guild === null ||
+      fetchedNewMessage.author === null ||
+      fetchedNewMessage.author.bot
+    ) {
+      return;
+    }
+
+    await log({
+      client,
+      type: LogType.MessageEdit,
+      guild,
+      reason: "",
+      moderator: fetchedNewMessage.author,
+      args: [oldMessage, fetchedNewMessage]
+    });
+  });
