6abda6a77ad9008fe3ab3db556f53ba6344728fa
Ferotiq <evanrayweigel@outlook.com>
2023-02-18 16:07:47 -0600

    add member ban and unban listeners

---
+++ src/listeners/on-member-ban.ts
@@ -0,0 +1,38 @@
+import { EventListener } from "fero-dc";
+import { log } from "../util/logging";
+import { LogType } from "../util/types";
+import { AuditLogEvent } from "discord.js";
+
+export default new EventListener<"guildBanAdd">()
+	.setEvent("guildBanAdd")
+	.setListener(async (client, ban) => {
+		const guild = ban.guild;
+		const user = ban.user;
+
+		const auditLog = await guild
+			.fetchAuditLogs({
+				limit: 1,
+				type: AuditLogEvent.MemberBanAdd
+			})
+			.then((auditLogs) => auditLogs.entries.first())
+			.catch(() => undefined);
+		if (auditLog === undefined) {
+			return;
+		}
+
+		if (auditLog.executor?.id === client.user.id) {
+			return;
+		}
+
+		const moderator = auditLog.executor ?? client.user;
+		const reason = auditLog.reason ?? "No reason provided";
+
+		await log({
+			client,
+			type: LogType.Ban,
+			guild,
+			reason,
+			moderator,
+			args: [user]
+		});
+	});
---
+++ src/listeners/on-member-unban.ts
@@ -0,0 +1,38 @@
+import { EventListener } from "fero-dc";
+import { log } from "../util/logging";
+import { LogType } from "../util/types";
+import { AuditLogEvent } from "discord.js";
+
+export default new EventListener<"guildBanRemove">()
+	.setEvent("guildBanRemove")
+	.setListener(async (client, ban) => {
+		const guild = ban.guild;
+		const user = ban.user;
+
+		const auditLog = await guild
+			.fetchAuditLogs({
+				limit: 1,
+				type: AuditLogEvent.MemberBanRemove
+			})
+			.then((auditLogs) => auditLogs.entries.first())
+			.catch(() => undefined);
+		if (auditLog === undefined) {
+			return;
+		}
+
+		if (auditLog.executor?.id === client.user.id) {
+			return;
+		}
+
+		const moderator = auditLog.executor ?? client.user;
+		const reason = auditLog.reason ?? "No reason provided";
+
+		await log({
+			client,
+			type: LogType.Unban,
+			guild,
+			reason,
+			moderator,
+			args: [user]
+		});
+	});
